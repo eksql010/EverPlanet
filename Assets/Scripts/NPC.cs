9c903841837a6604f2243c8d31a589b1bb5bb624
70792497+eksql010@users.noreply.github.com <70792497+eksql010@users.noreply.github.com>
2026-09-26 00:07:48 +0900

    feat : NPC와 상호작용 시 퀘스트 대화창 활성화/비활성화 기능 추가

--- Assets/Scripts/NPC.cs
+++ Assets/Scripts/NPC.cs
@@ -3,16 +3,19 @@ using UnityEngine;
 public class NPC : MonoBehaviour, IInteractable
 {
     [SerializeField] private QuestData quest;
+    [SerializeField] private QuestDialogueWindow dialogueWindow;
 
     public void Interact()
     {
-        QuestState state = QuestManager.instance.GetState(quest.questId);
+        //  QuestState state = QuestManager.instance.GetState(quest.questId);
+        //  
+        //  if (state == QuestState.NotStarted)
+        //      QuestManager.instance.AcceptQuest(quest);
+        //  else if (state == QuestState.ObjectiveComplete)
+        //      QuestManager.instance.CompleteQuest(quest.questId);
+        //  else
+        //      Debug.Log("현재 퀘스트 상태 : " + state.ToString());
 
-        if (state == QuestState.NotStarted)
-            QuestManager.instance.AcceptQuest(quest);
-        else if (state == QuestState.ObjectiveComplete)
-            QuestManager.instance.CompleteQuest(quest.questId);
-        else
-            Debug.Log("현재 퀘스트 상태 : " + state.ToString());
+        dialogueWindow.Open();
     }
 }
--- Assets/Scripts/QuestData.cs
+++ Assets/Scripts/QuestData.cs
@@ -8,4 +8,9 @@ public class QuestData : ScriptableObject
     public string description;
     public int rewardExp;
     public int rewardMoney;
+
+    [TextArea] public string[] introDialogue;               // 미수락 상태에서 말 걸 때
+    [TextArea] public string[] inProgressDialogue;          // 퀘스트 진행 중 말 걸 때
+    [TextArea] public string[] objectiveCompleteDialogue;   // 목표 달성 후 말 걸 때
+    [TextArea] public string[] completedDialogue;           // 완료 후 말 걸 때
 }
